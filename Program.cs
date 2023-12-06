5a336403d2af29c3e15df480a2e48be54a065a5d
SBICZA01\A253559 <Lesedi.Shirami@standardbank.co.za>
2023-12-06 14:32:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getting_user_input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+            Console.WriteLine("How old are you?");
+
+            String age = Console.ReadLine();
+            Console.WriteLine(name + " is " + age + " years old");
+            
+            Console.ReadLine();
+        }
+    }
+}
